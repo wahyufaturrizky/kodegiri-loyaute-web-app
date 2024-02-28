9ea45bc58e1dcc276d225fb22354ddc6c9ef1944
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-02-28 13:14:04 +0700

    FEAT: mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,10 +130,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col">
-      <section className="bg-section-welcome h-lvh">
+      <section className="bg-section-welcome bg-center sm:h-[689px] h-[380px]">
         {/* Start Header */}
-        <div className="sm:mx-auto sm:w-full sm:max-w-5xl p-6">
-          <div className="flex items-center justify-between">
+        <div className="sm:mx-auto sm:w-full sm:max-w-5xl sm:p-6 p-1">
+          <div className="flex sm:flex-row gap-4 flex-col items-center justify-between">
             <ImageNext
               src="/logo.svg"
               width={156.24}
@@ -143,20 +143,20 @@ export default function Home() {
               className="h-auto w-auto"
             />
 
-            <div className="flex items-center gap-12">
+            <div className="flex items-center sm:gap-12 gap-4">
               {navMenu.map((item) => (
                 <div
                   key={item.label}
-                  className="flex group flex-col justify-center items-center gap-2 cursor-pointer"
+                  className="flex group flex-col justify-center items-center sm:gap-2 gap-4 cursor-pointer"
                 >
                   {createElement(item.icon, {
                     className:
-                      "text-white group-hover:text-blue-primary group-active:text-blue-primary/70",
+                      "text-white group-hover:text-blue-primary group-active:text-blue-primary/70 h-auto w-auto",
                   })}
 
                   <Text
                     label={item.label}
-                    className="text-white group-hover:text-blue-primary group-active:text-blue-primary/70"
+                    className="text-white text-xs sm:text-base group-hover:text-blue-primary group-active:text-blue-primary/70"
                   />
                 </div>
               ))}
@@ -169,11 +169,11 @@ export default function Home() {
                 <div>
                   <Text
                     label="Good Morning,"
-                    className="text-white font-light text-sm text-right"
+                    className="text-white font-light sm:text-sm text-xs text-right"
                   />
                   <Text
                     label="Wahyu Fatur Rizki"
-                    className="text-white font-bold text-base text-right"
+                    className="text-white font-bold sm:text-base text-sm text-right"
                   />
                 </div>
 
@@ -183,7 +183,7 @@ export default function Home() {
                   height={48}
                   priority
                   alt="avatar"
-                  className="h-[48px] w-auto"
+                  className="sm:h-[48px] h-[32px] w-auto"
                 />
               </div>
             </div>
@@ -191,25 +191,25 @@ export default function Home() {
         </div>
         {/* End Header */}
 
-        <div className="flex flex-col mt-40 items-center">
+        <div className="flex flex-col sm:mt-10 mt-2 items-center">
           <div className="flex items-center gap-1">
-            <Text label="Welcome to" className="text-white text-7xl" />
+            <Text label="Welcome to" className="text-white text-2xl sm:text-7xl" />
 
-            <Text label="Loyauté" className="text-blue-primary text-7xl" />
+            <Text label="Loyauté" className="text-blue-primary text-2xl sm:text-7xl" />
           </div>
 
           <Text
             label="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-            className="text-white text-center sm:mx-auto sm:w-full sm:max-w-lg mt-6"
+            className="text-white text-center text-[10px] sm:text-base sm:mx-auto sm:w-full sm:max-w-lg sm:mt-6 mt-1"
           />
 
-          <div className="flex items-center gap-8 mt-12">
+          <div className="flex items-center sm:gap-8 gap-2 sm:mt-12 mt-4">
             <Button
               type="button"
               onClick={() => {}}
               icon={<PlayStoreIcon />}
               label="Play Store"
-              className="flex gap-2 border border-white justify-center items-center rounded-full px-6 py-1.5 text-lg text-white shadow-sm hover:border-blue-primary/70 active:border-blue-primary/90 hover:text-blue-primary/70 active:text-blue-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex gap-2 border border-white justify-center items-center rounded-full sm:px-6 px-4 py-1.5 sm:text-lg text-xs text-white shadow-sm hover:border-blue-primary/70 active:border-blue-primary/90 hover:text-blue-primary/70 active:text-blue-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             />
 
             <Button
@@ -217,7 +217,7 @@ export default function Home() {
               onClick={() => {}}
               icon={<AppleIcon />}
               label="Apps Store"
-              className="flex gap-2 border border-white justify-center items-center rounded-full px-6 py-1.5 text-lg text-white shadow-sm hover:border-blue-primary/70 active:border-blue-primary/90 hover:text-blue-primary/70 active:text-blue-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex gap-2 border border-white justify-center items-center rounded-full sm:px-6 px-4 py-1.5 sm:text-lg text-xs text-white shadow-sm hover:border-blue-primary/70 active:border-blue-primary/90 hover:text-blue-primary/70 active:text-blue-primary/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             />
           </div>
         </div>
@@ -225,14 +225,14 @@ export default function Home() {
 
       <section className="bg-pattern">
         <div className="sm:mx-auto sm:w-full sm:max-w-7xl p-6">
-          <Text label="Latest Promo 🔥" className="text-black text-3xl" />
+          <Text label="Latest Promo 🔥" className="text-black sm:text-3xl text-xl" />
           <Text
             label="Don't miss out! Limited time offer inside!"
-            className="text-[#959595] text-base"
+            className="text-[#959595] sm:text-base text-xs"
           />
         </div>
 
-        <div className="relative h-[300px]">
+        <div className="relative sm:h-[300px] h-auto">
           <Swiper
             pagination={true}
             navigation={true}
@@ -253,20 +253,31 @@ export default function Home() {
       <section>
         <div className="sm:mx-auto sm:w-full sm:max-w-5xl p-6">
           <div className="flex items-center justify-between">
-            <Text label="Popular Merchant" className="text-black text-3xl" />
-            <Text label="view more" className="text-blue-link text-base" />
+            <Text label="Popular Merchant" className="text-black sm:text-3xl text-xl" />
+            <Text label="view more" className="text-blue-link sm:text-base text-xs" />
           </div>
 
-          <div className="flex items-center mt-12 justify-between">
+          <div className="flex items-center sm:mt-12 mt-6 justify-between">
             {merchant.map((item) => (
-              <ImageNext key={item} alt="merchant" width={100} height={100} priority src={item} />
+              <ImageNext
+                key={item}
+                alt="merchant"
+                className="h-auto sm:w-auto w-[30px]"
+                width={100}
+                height={100}
+                priority
+                src={item}
+              />
             ))}
           </div>
 
-          <Text label="Popular Rewards" className="text-black text-3xl mt-12" />
-          <Text label="Explore our latest popular rewards!" className="text-[#959595] text-base" />
+          <Text label="Popular Rewards" className="text-black sm:text-3xl text-xl sm:mt-12 mt-6" />
+          <Text
+            label="Explore our latest popular rewards!"
+            className="text-[#959595] sm:text-base text-xs"
+          />
 
-          <div className="flex flex-wrap gap-6 mt-12">
+          <div className="flex flex-wrap gap-6 sm:mt-12 mt-4">
             {category.map((item) => (
               <div
                 key={item.label}
@@ -274,7 +285,7 @@ export default function Home() {
                   item.label === "All" ? "bg-blue-primary text-white" : "bg-[#F6F7FB] text-black"
                 } rounded-full py-2 px-4  `}
               >
-                <Text label={item.label} className="text-base font-light" />
+                <Text label={item.label} className="sm:text-base text-xs font-light" />
               </div>
             ))}
           </div>
@@ -287,11 +298,11 @@ export default function Home() {
                   width={228}
                   height={228}
                   priority
-                  className="h-[228px] w-auto"
+                  className="sm:h-[228px] h-auto w-auto"
                   src={`/reward-0${index + 1}.png`}
                 />
 
-                <div className="flex items-center gap-2 mt-4">
+                <div className="flex sm:flex-row flex-col sm:items-center items-start sm:gap-2 gap-1.5 mt-4">
                   <ImageNext
                     alt={item.merchantTitle}
                     width={24}
@@ -304,9 +315,12 @@ export default function Home() {
                   <Text label={item.merchantTitle} className="text-black font-light text-xs" />
                 </div>
 
-                <Text label={item.title} className="text-black mt-2" />
+                <Text label={item.title} className="text-black sm:text-base text-xs mt-2" />
 
-                <Text label="Point earned" className="text-[#979797] mt-2 font-light" />
+                <Text
+                  label="Point earned"
+                  className="text-[#979797] sm:text-base text-xs mt-2 font-light"
+                />
 
                 <div className="flex items-center gap-2 mt-1">
                   <ImageNext
@@ -314,11 +328,14 @@ export default function Home() {
                     width={19}
                     height={19}
                     priority
-                    className="h-[19px] w-auto"
+                    className="sm:h-[19px] h-[12px] w-auto"
                     src="/ic-point.svg"
                   />
 
-                  <Text label={String(item.point)} className="text-black font-light" />
+                  <Text
+                    label={String(item.point)}
+                    className="text-black sm:text-base text-xs font-light"
+                  />
                 </div>
               </div>
             ))}
@@ -326,7 +343,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section></section>
+      <section className="bg-instant h-[512px] bg-center"></section>
     </main>
   );
 }
